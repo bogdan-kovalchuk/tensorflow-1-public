7d780320645e9665bd516a8f4701bca38e2a8291
bogdan-kovalchuk <35914116+bogdan-kovalchuk@users.noreply.github.com>
2023-08-27 10:00:00 +0300

    Validate notebook document structure

--- notebook_check.py
+++ notebook_check.py
@@ -4,6 +4,64 @@ import re
 import sys
 
 
+class NotebookValidationError(ValueError):
+    pass
+
+
+def _expect_type(value, expected_type, location):
+    if not isinstance(value, expected_type):
+        raise NotebookValidationError(
+            "{} must be {}, got {}".format(
+                location, expected_type.__name__, type(value).__name__
+            )
+        )
+
+
+def validate_notebook_structure(notebook):
+    _expect_type(notebook, dict, "notebook")
+    _expect_type(notebook.get("cells"), list, "cells")
+    _expect_type(notebook.get("metadata"), dict, "metadata")
+    _expect_type(notebook.get("nbformat"), int, "nbformat")
+    _expect_type(notebook.get("nbformat_minor"), int, "nbformat_minor")
+    if notebook["nbformat"] != 4:
+        raise NotebookValidationError(
+            "nbformat must be 4, got {}".format(notebook["nbformat"])
+        )
+
+    for index, cell in enumerate(notebook["cells"]):
+        location = "cell {}".format(index)
+        _expect_type(cell, dict, location)
+        if cell.get("cell_type") not in {"code", "markdown", "raw"}:
+            raise NotebookValidationError(
+                "{} has unsupported cell_type: {}".format(
+                    location, cell.get("cell_type")
+                )
+            )
+        source = cell.get("source")
+        if not isinstance(source, (str, list)):
+            raise NotebookValidationError("{} source must be text or a list".format(location))
+        if isinstance(source, list) and any(not isinstance(line, str) for line in source):
+            raise NotebookValidationError("{} source contains a non-string item".format(location))
+        _expect_type(cell.get("metadata"), dict, "{} metadata".format(location))
+        if cell["cell_type"] == "code":
+            _expect_type(cell.get("outputs"), list, "{} outputs".format(location))
+            execution_count = cell.get("execution_count")
+            if execution_count is not None and not isinstance(execution_count, int):
+                raise NotebookValidationError(
+                    "{} execution_count must be an integer or null".format(location)
+                )
+    return notebook
+
+
+def load_notebook(nb_path):
+    try:
+        with open(nb_path, "r", encoding="utf-8") as notebook_file:
+            notebook = json.load(notebook_file)
+    except (OSError, UnicodeError, json.JSONDecodeError) as error:
+        raise NotebookValidationError(str(error)) from error
+    return validate_notebook_structure(notebook)
+
+
 def find_notebooks(root):
     notebooks = []
     for dirpath, _, filenames in os.walk(root):
@@ -14,8 +72,7 @@ def find_notebooks(root):
 
 
 def extract_graded_functions(nb_path):
-    with open(nb_path, "r", encoding="utf-8") as f:
-        nb = json.load(f)
+    nb = load_notebook(nb_path)
     functions = []
     for cell in nb.get("cells", []):
         if cell.get("cell_type") != "code":
@@ -35,8 +92,7 @@ def main():
     for nb_path in notebooks:
         rel = os.path.relpath(nb_path, root)
         try:
-            with open(nb_path, "r", encoding="utf-8") as f:
-                json.load(f)
+            load_notebook(nb_path)
             funcs = extract_graded_functions(nb_path)
             total_funcs += len(funcs)
             tag = f"  graded: {', '.join(funcs)}" if funcs else ""
